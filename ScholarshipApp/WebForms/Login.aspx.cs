71a112b056e259eb7dccc5322919418d0187ab3f
Hamza Khan <hamzalko9@gmail.com>
2024-02-28 20:19:16 +0530

    Passing ConnectStrings from Web.config file.

--- ScholarshipApp/WebForms/Login.aspx.cs
+++ ScholarshipApp/WebForms/Login.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.EnterpriseServices;
@@ -11,59 +12,59 @@ using System.Web.UI.WebControls;
 
 public partial class Login : System.Web.UI.Page
 {
-    SqlConnection bridge;
+	SqlConnection bridge;
 
-    public Login()
-    {
-        bridge = new SqlConnection("Data Source= LAPTOP-I52TFVIL; Initial Catalog= Scholarship;Integrated Security= True");
-    }
-    protected void Page_Load(object sender, EventArgs e)
-    {
+	public Login()
+	{
+		bridge = new SqlConnection(ConfigurationManager.ConnectionStrings["con"].ConnectionString);
+	}
+	protected void Page_Load(object sender, EventArgs e)
+	{
 
 
-    }
+	}
 
-    protected void btnLogin_Click(object sender, EventArgs e)
-    {
-        try
-        {
-            DataTable data = new DataTable();
-            string query = @"SELECT * FROM UserDetails WHERE UserEmail=@UserEmail AND Password=@Password";
-            SqlCommand command = new SqlCommand(query, bridge);
-            command.Parameters.Clear();
-            command.Parameters.AddWithValue("@UserEmail", txtUserEmail.Text);
-            command.Parameters.AddWithValue("@Password", txtPassword.Text);
-            if (bridge.State != System.Data.ConnectionState.Open)
-                bridge.Open();
+	protected void btnLogin_Click(object sender, EventArgs e)
+	{
+		try
+		{
+			DataTable data = new DataTable();
+			string query = @"SELECT * FROM UserDetails WHERE UserEmail=@UserEmail AND Password=@Password";
+			SqlCommand command = new SqlCommand(query, bridge);
+			command.Parameters.Clear();
+			command.Parameters.AddWithValue("@UserEmail", txtUserEmail.Text);
+			command.Parameters.AddWithValue("@Password", txtPassword.Text);
+			if (bridge.State != System.Data.ConnectionState.Open)
+				bridge.Open();
 
-            SqlDataAdapter adapter = new SqlDataAdapter(command);
-            adapter.Fill(data);
+			SqlDataAdapter adapter = new SqlDataAdapter(command);
+			adapter.Fill(data);
 
-            if (data.Rows.Count > 0)
-            {
-                Session["UserName"] = data.Rows[0]["UserName"].ToString();
+			if (data.Rows.Count > 0)
+			{
+				Session["UserName"] = data.Rows[0]["UserName"].ToString();
 
-                string roleID = data.Rows[0]["RoleID"].ToString();
-                if (roleID.Equals("1"))
-                    Response.Redirect("AdminDashboard.aspx");
-                else if (roleID.Equals("2"))
-                    Response.Redirect("ApproverDashboard.aspx");
-                else
-                    Response.Redirect("StudentDashboard.aspx");
-            }
-            else
-            {
-                string Title = "Invalid";
-                string Message = "Please enter correct credentials";
-                ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "Script", "swal('" + Title + "','" + Message + "','warning');", true);
-            }
+				string roleID = data.Rows[0]["RoleID"].ToString();
+				if (roleID.Equals("1"))
+					Response.Redirect("AdminDashboard.aspx");
+				else if (roleID.Equals("2"))
+					Response.Redirect("ApproverDashboard.aspx");
+				else
+					Response.Redirect("StudentDashboard.aspx");
+			}
+			else
+			{
+				string Title = "Invalid";
+				string Message = "Please enter correct credentials";
+				ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "Script", "swal('" + Title + "','" + Message + "','warning');", true);
+			}
 
-        }
-        catch (Exception ex)
-        {
-            string Title = "Error";
-            string Message = "Something went wrong. Please contact to admin";
-            ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "Script", "swal('" + Title + "','" + Message + "','error');", true);
-        }
-    }
+		}
+		catch (Exception ex)
+		{
+			string Title = "Error";
+			string Message = "Something went wrong. Please contact to admin";
+			ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "Script", "swal('" + Title + "','" + Message + "','error');", true);
+		}
+	}
 }
--- ScholarshipApp/WebForms/Registration.aspx.cs
+++ ScholarshipApp/WebForms/Registration.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Activities.Statements;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -12,52 +13,52 @@ public partial class Registration : System.Web.UI.Page
 	SqlConnection bridge;
 	public Registration()
 	{
-		bridge = new SqlConnection("Data Source= LAPTOP-I52TFVIL; Initial Catalog= Scholarship;Integrated Security= True");
+		bridge = new SqlConnection(ConfigurationManager.ConnectionStrings["con"].ConnectionString);
 	}
 	protected void Page_Load(object sender, EventArgs e)
 	{
 
 	}
 
-    protected void btnSignup_Click(object sender, EventArgs e)
-    {
-        try
-        {
-            string userName = txtUsername.Text;
-            string eMail = txtEmail.Text;
-            string password = txtPassword.Text;
-            string mobile = txtMobile.Text;
+	protected void btnSignup_Click(object sender, EventArgs e)
+	{
+		try
+		{
+			string userName = txtUsername.Text;
+			string eMail = txtEmail.Text;
+			string password = txtPassword.Text;
+			string mobile = txtMobile.Text;
 
 
-            string query = @"Insert into UserDetails values(@UserName,@UserEmail,@Password,@UserMobile,@RoleID)";
-            SqlCommand command = new SqlCommand(query, bridge);
-            command.Parameters.Clear();
-            command.Parameters.AddWithValue("@UserName", userName);
-            command.Parameters.AddWithValue("@UserEmail", eMail);
-            command.Parameters.AddWithValue("@Password", password);
-            command.Parameters.AddWithValue("@UserMobile", mobile);
-            command.Parameters.AddWithValue("@RoleID", 3);
-            if (bridge.State != System.Data.ConnectionState.Open)
-                bridge.Open();
+			string query = @"Insert into UserDetails values(@UserName,@UserEmail,@Password,@UserMobile,@RoleID)";
+			SqlCommand command = new SqlCommand(query, bridge);
+			command.Parameters.Clear();
+			command.Parameters.AddWithValue("@UserName", userName);
+			command.Parameters.AddWithValue("@UserEmail", eMail);
+			command.Parameters.AddWithValue("@Password", password);
+			command.Parameters.AddWithValue("@UserMobile", mobile);
+			command.Parameters.AddWithValue("@RoleID", 3);
+			if (bridge.State != System.Data.ConnectionState.Open)
+				bridge.Open();
 
-            int noofrowseffected = command.ExecuteNonQuery();
-            if (noofrowseffected > 0)
-            {
-                string Title = "Success";
-                string Message = "User is registered now";
-                ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "Script", "swal('" + Title + "','" + Message + "','success');", true);
-                ClearControls();
-            }
-        }
-        catch (Exception ex)
-        {
-            string Title = "Error";
-            string Message = "Something went wrong. Please contact to admin";
-            ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "Script", "swal('" + Title + "','" + Message + "','error');", true);
+			int noofrowseffected = command.ExecuteNonQuery();
+			if (noofrowseffected > 0)
+			{
+				string Title = "Success";
+				string Message = "User is registered now";
+				ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "Script", "swal('" + Title + "','" + Message + "','success');", true);
+				ClearControls();
+			}
+		}
+		catch (Exception ex)
+		{
+			string Title = "Error";
+			string Message = "Something went wrong. Please contact to admin";
+			ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "Script", "swal('" + Title + "','" + Message + "','error');", true);
 
-        }
-       
-    }
+		}
+
+	}
 
 	private void ClearControls()
 	{
